ad01e9c83ee510fc1a9ac5c61604f83e6bccf760
howardotuya <howardotuya17@gmail.com>
2023-12-27 16:32:53 +0100

    update - oppia case study upto next case study

--- app/case-study/oppia/page.js
+++ app/case-study/oppia/page.js
@@ -20,6 +20,9 @@ import ds1 from "@/public/images/PNG/oppia/ds1.png";
 import ds2 from "@/public/images/PNG/oppia/ds2.png";
 import longArrow from "@/public/images/SVG/oppia/longArrow.svg";
 import ut from "@/public/images/PNG/oppia/ut.png";
+import ut2 from "@/public/images/PNG/oppia/ut2.png";
+import Footer from "@/components/footer/footer";
+import SubFooter from "@/components/footer/subFooter";
 
 export default function Oppia() {
   return (
@@ -34,17 +37,17 @@ export default function Oppia() {
         {/* HERO SECTION */}
         <section className="bg-[#EFFEF3]">
           <Container>
-            <article className="pt-10 flex justify-center items-end gap-8">
+            <article className="pt-10 flex justify-center items-end gap-4 lg:gap-8">
               {/* FIRST IMAGE */}
-              <article className="max-w-[173px] w-full h-auto">
+              <article className="max-w-[90px] lg:max-w-[173px] w-full h-auto">
                 <Image src={oppia1} alt="" />
               </article>
               {/* SECOND IMAGE */}
-              <article className="max-w-[173px] w-full h-auto">
+              <article className="max-w-[90px] lg:max-w-[173px] w-full h-auto">
                 <Image src={oppia2} alt="" />
               </article>
               {/* THIRD IMAGE */}
-              <article className="max-w-[173px] w-full h-auto">
+              <article className="max-w-[90px] lg:max-w-[173px] w-full h-auto">
                 <Image src={oppia3} alt="" />
               </article>
             </article>
@@ -55,17 +58,17 @@ export default function Oppia() {
         <section className="px-5 lg:pl-[120px] ">
           <Container>
             {/* HEADING + Project OVERVIEW */}
-            <section className="lg:pr-[120px] pt-20 pb-11 flex flex-col gap-10">
-              <h1 className="text-[72px] font-medium leading-[100%] tracking-[-2.88px]">
+            <section className="lg:pr-[120px] pt-10 lg:pt-20 pb-5 lg:pb-11 flex flex-col gap-4 lg:gap-10">
+              <h1 className="text-[32px] lg:text-[72px] font-medium leading-[120%] lg:leading-[100%] tracking-[-1.28px] lg:tracking-[-2.88px]">
                 Oppia Beta Android App
               </h1>
 
               {/* PROJECT OVERVIEW */}
-              <article className="flex flex-col gap-6">
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   Project overview
                 </h2>
-                <p className="text-[18px] tracking-[-0.18px]">
+                <p className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   Oppia Foundation is a platform that provides education for
                   those who lack access to it. Statistics shows that 61 million
                   children aren’t able to attend primary education worldwide and
@@ -76,15 +79,15 @@ export default function Oppia() {
             </section>
 
             {/* PROBLEM STATEMENT + GOAL + IMAGE */}
-            <article className="lg:pr-[120px] grid grid-cols-[1fr_auto] items-center gap-6 ">
+            <article className="lg:pr-[120px] pt-8 pb-5 lg:py-0 grid lg:grid-cols-[1fr_auto] items-center gap-4 lg:gap-6 ">
               {/* PROBLEM STATEMENT AND GOAL */}
-              <article className="flex flex-col gap-10">
+              <article className="flex flex-col gap-4 lg:gap-10">
                 {/* PROBLEM STATEMENT */}
-                <article className="flex flex-col gap-6">
-                  <h2 className="cod-gray-800 text-[40px] font-semibold">
+                <article className="flex flex-col gap-4 lg:gap-6">
+                  <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                     Problem Statement
                   </h2>
-                  <p className="text-[18px] tracking-[-0.18px]">
+                  <p className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                     A lot of underdeveloped children in Nigeria and around the
                     world do not understand English language. Oppia is looking
                     to include language translation feature in order to reach a
@@ -93,11 +96,11 @@ export default function Oppia() {
                 </article>
 
                 {/* GOAL */}
-                <article className="flex flex-col gap-6">
-                  <h2 className="cod-gray-800 text-[40px] font-semibold">
+                <article className="flex flex-col gap-4 lg:gap-6">
+                  <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                     Goal
                   </h2>
-                  <p className="text-[18px] tracking-[-0.18px]">
+                  <p className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                     The goal is to simplify the experience of the app and make
                     it easily accessible to every underprivileged children
                     around the world.
@@ -112,13 +115,13 @@ export default function Oppia() {
             </article>
 
             {/* RESEARCH AND ANALYSIS */}
-            <article className="lg:pr-[120px] flex flex-col gap-6 py-10">
+            <article className="lg:pr-[120px] pt-8 pb-5 flex flex-col gap-5 lg:gap-6 lg:py-10">
               {/* RAA 1.1 */}
-              <article className="flex flex-col gap-6">
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   Research & Analysis
                 </h2>
-                <p className="max-w-[807px] text-[18px] tracking-[-0.18px]">
+                <p className="max-w-[807px] text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   Before coming up with the solution, I conducted interview with
                   children within the range of 7-14. Here are the interview
                   questions and key insights from the research
@@ -126,15 +129,15 @@ export default function Oppia() {
               </article>
 
               {/* RAA 2.2 */}
-              <article className="grid grid-cols-[auto_1fr] gap-[72px]">
+              <article className="grid gap-5 lg:grid-cols-[auto_1fr] lg:gap-[72px]">
                 {/* RAA 2.2.1 - Interview Questions */}
-                <article className="flex flex-col gap-4">
+                <article className="flex flex-col gap-5 lg:gap-4">
                   {/* HEADING */}
-                  <h5 className="text-[24px] font-medium">
+                  <h5 className="text-[16px] lg:text-[24px] font-semibold lg:font-medium">
                     Interview Questions
                   </h5>
                   {/* LISTING */}
-                  <article className="text-[18px] tracking-[-0.18px] flex flex-col gap-4">
+                  <article className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px] flex flex-col gap-4">
                     <p>1. Did you find it difficult reading in English?</p>
                     <p>
                       2. What are some of the distractions you face when using
@@ -146,11 +149,13 @@ export default function Oppia() {
                 </article>
 
                 {/* RAA 2.2.2 - Key Insights */}
-                <article className="flex flex-col gap-4">
+                <article className="flex flex-col gap-5 lg:gap-4">
                   {/* HEADING */}
-                  <h5 className="text-[24px] font-medium">Key Insights</h5>
+                  <h5 className="text-[16px] lg:text-[24px] font-semibold lg:font-medium">
+                    Key Insights
+                  </h5>
                   {/* LISTING */}
-                  <article className="text-[18px] tracking-[-0.18px] flex flex-col gap-4">
+                  <article className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px] flex flex-col gap-4">
                     <p>
                       1. 30% of the mobile app users needed interpreters to help
                       translate{" "}
@@ -164,13 +169,13 @@ export default function Oppia() {
             </article>
 
             {/* Our Process using the “How Might We Technique” */}
-            <article className="pt-10 pb-[134px] flex flex-col gap-[76px]">
+            <article className="pt-8 pb-14 gap-9 lg:pt-10 lg:pb-[134px] flex flex-col lg:gap-[76px]">
               {/* 1.1 */}
-              <article className="flex flex-col gap-6">
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   Our Process using the “How Might We Technique”
                 </h2>
-                <p className="max-w-[807px] text-[18px] tracking-[-0.18px]">
+                <p className="max-w-[807px] text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   At the end of the discovery, we constructed some how might we
                   questions that will help us solve frame the problems for
                   ideation.
@@ -246,15 +251,15 @@ export default function Oppia() {
             </article>
 
             {/* DESIGN SOLUTION */}
-            <article className="py-10 flex flex-col gap-10">
+            <article className="pt-8 pb-5 lg:py-10 flex flex-col gap-6 lg:gap-10">
               {/* HEADING + SUBHEADING */}
-              <article className="flex flex-col gap-6">
+              <article className="flex flex-col gap-4 lg:gap-6">
                 {/* HEADING */}
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   Design Solution
                 </h2>
                 {/* SUB HEADING */}
-                <p className="max-w-[807px] text-[18px] tracking-[-0.18px]">
+                <p className="max-w-[807px] text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   We implemented the <b>language feature</b> and added{" "}
                   <b>three languages</b>( Portuguese, Arabic, Pidgin )
                 </p>
@@ -263,16 +268,20 @@ export default function Oppia() {
               {/* IMAGERY */}
               <article className="flex items-center self-center">
                 {/* IMAGE 1 */}
-                <article className="max-w-[234px] w-full h-auto">
+                <article className="w-[137px] max-w-[234px] lg:w-full h-auto shrink-0">
                   <Image src={ds1} alt="" />
                 </article>
 
                 {/* CONTENT - MIDDLE */}
-                <article className="pl-2">
-                  <p className="text-[16px] tracking-[-0.16px] max-w-[288px]">
+                <article className="">
+                  <p className="hidden lg:block pl-2 text-[16px] tracking-[-0.16px] max-w-[288px]">
                     Once the user clicks on the App language in the settings
                     page, it takes the user to the language option and they are
                     able to switch languages
+                  </p>
+                  <p className="block lg:hidden pl-2 text-[12px]">
+                    Once a user clicks on App language, It takes them to the
+                    Language options
                   </p>
                   {/* IMAGE */}
                   <article>
@@ -281,36 +290,37 @@ export default function Oppia() {
                 </article>
 
                 {/* IMAGE 2 */}
-                <article className="max-w-[234px] w-full h-auto">
+                <article className="w-[137px] max-w-[234px] lg:w-full h-auto shrink-0">
                   <Image src={ds2} alt="" />
                 </article>
               </article>
             </article>
 
             {/* USER TESTING */}
-            <article className="py-10 lg:pr-[120px]">
+            <article className="pt-8 pb-5 lg:py-10 lg:pr-[120px]">
               {/* HEADING + TEXT */}
-              <article className="flex flex-col gap-6">
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   User Testing
                 </h2>
-                <p className="max-w-[807px] text-[18px] tracking-[-0.18px]">
+                <p className="max-w-[807px] text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   I conducted an in person interview with two children to be
                   sure they can access the language feature and switch to the
                   language of their choice
                 </p>
               </article>
               {/* IMAGERY */}
-              <article className="mt-8 mb-10">
-                <Image className="w-full h-auto" src={ut} alt="" />
+              <article className="mt-4 mb-6 lg:mt-8 lg:mb-10">
+                <Image className="hidden lg:block w-full h-auto" src={ut} alt="" />
+                <Image className="block lg:hidden w-full h-auto" src={ut2} alt="" />
               </article>
 
               {/* SUBHEADING + TEXTS */}
-              <article className="flex flex-col gap-6">
-                <h4 className="text-[24px] font-semibold cod-gray-800">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h4 className="text-[18px] lg:text-[24px] font-semibold cod-gray-800">
                   Results from the testing
                 </h4>
-                <article className="flex flex-col gap-6 text-[18px] tracking-[-0.18px]">
+                <article className="flex flex-col gap-6 text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   <p>
                     From the testing, we gathered that users should be able to
                     choose the language they want at the onboarding stage.
@@ -322,13 +332,13 @@ export default function Oppia() {
             </article>
 
             {/* Product Metrics */}
-            <article className="max-w-[1077px] py-10 lg:pr-[120px]">
+            <article className="max-w-[1077px] pt-8 pb-5 lg:py-10 lg:pr-[120px]">
               {/* HEADING + TEXT */}
-              <article className="flex flex-col gap-6">
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   Product Metrics
                 </h2>
-                <p className="text-[18px] tracking-[-0.18px]">
+                <p className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   Currently we have 4 languages on the app, and through our
                   analytics we have gotten 10,000 more children onboarded on the
                   app and we are looking to add more languages on the app.
@@ -337,13 +347,13 @@ export default function Oppia() {
             </article>
 
             {/* What Comes Next? */}
-            <article className="max-w-[1077px] py-10 lg:pr-[120px]">
+            <article className="max-w-[1077px] pt-8 pb-5 lg:py-10 lg:pr-[120px]">
               {/* HEADING + TEXT */}
-              <article className="flex flex-col gap-6">
-                <h2 className="cod-gray-800 text-[40px] font-semibold">
+              <article className="flex flex-col gap-4 lg:gap-6">
+                <h2 className="cod-gray-800 text-[24px] lg:text-[40px] font-semibold">
                   What Comes Next?
                 </h2>
-                <p className="text-[18px] tracking-[-0.18px]">
+                <p className="text-[14px] lg:text-[18px] leading-[140%] lg:leading-normal lg:tracking-[-0.18px]">
                   We will keep improving the experience of the app and reaching
                   out to NGOs to help train underprivileged children to be able
                   to use the app.
@@ -352,7 +362,13 @@ export default function Oppia() {
             </article>
           </Container>
         </section>
+
+        {/* NEXT PROJECT */}
+        <SubFooter first={"Jogovest"} link={"/"} />
       </main>
+
+      {/* FOOTER */}
+      <Footer />
     </>
   );
 }
---
+++ components/footer/subFooter.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Container from "../container/container";
+import next from "@/public/images/SVG/next.svg";
+import Link from "next/link";
+
+export default function SubFooter({ first, link }) {
+  return (
+    <>
+      <Link href={link} className="bg-[#7BFAAD] block">
+        <Container>
+          <article className="py-6 px-5 lg:py-10 lg:px-[120px] flex justify-between items-center">
+            {/* NEXT CASE STUDY */}
+            <h3 className="text-[20px] lg:text-[40px] font-medium tracking-[-0.2px] lg:tracking-[-0.4px]">
+              NEXT CASE STUDY
+            </h3>
+            {/* JOGOVEST */}
+            <article className="flex items-center gap-3">
+              <h3 className="text-[20px] lg:text-[40px] font-medium tracking-[-0.2px] lg:tracking-[-0.4px]">
+                {first}
+              </h3>
+              <Image className="w-8 h-8 lg:w-auto lg:h-auto" src={next} alt="" />
+            </article>
+          </article>
+        </Container>
+      </Link>
+    </>
+  );
+}
