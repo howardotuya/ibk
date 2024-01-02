50726e116899969d9d616e2ef86d0e19c4558b26
howardotuya <howardotuya17@gmail.com>
2024-01-02 23:32:40 +0100

    update - Testimonials v4

--- components/testimonials/testimonials.js
+++ components/testimonials/testimonials.js
@@ -5,9 +5,22 @@ import arrowRight from "@/public/images/SVG/arrow-right.svg";
 import arrowLeft from "@/public/images/SVG/arrow-left.svg";
 import Image from "next/image";
 import { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 export default function Testimonials() {
   let [number, setNumber] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
 
   // ARRAY CONTAINING THE TESTIMONIALS
   const Testimonials = [
@@ -50,56 +63,64 @@ export default function Testimonials() {
         </article>
 
         {/* CENTER CONTENT */}
-        <article className="pt-2 lg:pt-6 flex flex-col gap-6 lg:gap-10">
-          {/* TOP - TEXT */}
+        <article className="pt-2 lg:pt-6 grid gap-6 lg:gap-10">
+          <div ref={sliderRef} className="keen-slider flex flex-nowrap w-full">
+            {Testimonials.map((x) => (
+              <div
+                key={x.id}
+                className={`keen-slider__slide number-slide${
+                  x.id + 1
+                } w-full basis-[100%] grow shrink-0`}
+              >
+                <article className="flex flex-col gap-4 lg:gap-6">
+                  <p className="relative text-[12px] lg:text-[18px] leading-normal lg:leading-[140%]">
+                    {x.comment}
+                  </p>
 
-          {/* absolute - main */}
-          <article className="flex flex-col gap-4 lg:gap-6">
-            <p className="relative text-[12px] lg:text-[18px] leading-normal lg:leading-[140%]">
-              {Testimonials[number].comment}
-            </p>
+                  {/* NAME AND OCCUPATION */}
+                  <article className="flex flex-col gap-2">
+                    <h4 className="text-[16px] lg:text-[32px] font-bold leading-normal">
+                      {x.name}
+                    </h4>
+                    <p className="text-[16px] lg:text-[18px] ">
+                      {x.occupation}
+                    </p>
+                  </article>
+                </article>
+              </div>
+            ))}
+          </div>
 
-            {/* NAME AND OCCUPATION */}
-            <article className="flex flex-col gap-2">
-              <h4 className="text-[16px] lg:text-[32px] font-bold leading-normal">
-                {Testimonials[number].name}
-              </h4>
-              <p className="text-[16px] lg:text-[18px] ">
-                {Testimonials[number].occupation}
-              </p>
+          {loaded && instanceRef.current && (
+            <article className="flex gap-2 lg:gap-5">
+              {/* LEFT */}
+              <button
+                className="disabled:opacity-70 w-6 h-6 lg:w-auto lg:h-auto cursor-pointer"
+                onClick={(e) =>
+                  e.stopPropagation() || instanceRef.current?.prev()
+                }
+                disabled={currentSlide === 0}
+              >
+                <Image src={arrowLeft} alt="" left />
+              </button>
+
+              {/* RIGHT */}
+              <button
+                className="disabled:opacity-70 w-6 h-6 lg:w-auto lg:h-auto cursor-pointer"
+                onClick={(e) =>
+                  e.stopPropagation() || instanceRef.current?.next()
+                }
+                disabled={
+                  currentSlide ===
+                  instanceRef.current.track.details.slides.length - 1
+                }
+              >
+                <Image src={arrowRight} alt="" left />
+              </button>
             </article>
-          </article>
+          )}
 
           {/* BOTTOM - ARROW */}
-          <article className="flex gap-2 lg:gap-5">
-            {/* LEFT */}
-            <Image
-              className="w-6 h-6 lg:w-auto lg:h-auto cursor-pointer"
-              src={arrowLeft}
-              alt=""
-              onClick={() => {
-                if (number === 0) {
-                  setNumber(Testimonials.length - 1);
-                } else {
-                  setNumber(number - 1);
-                }
-              }}
-            />
-
-            {/* RIGHT */}
-            <Image
-              className="w-6 h-6 lg:w-auto lg:h-auto cursor-pointer"
-              src={arrowRight}
-              alt=""
-              onClick={() => {
-                if (number === Testimonials.length - 1) {
-                  setNumber(0);
-                } else {
-                  setNumber(number + 1);
-                }
-              }}
-            />
-          </article>
         </article>
 
         {/* RIGHT */}
