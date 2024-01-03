0d5e54d9770a602850ae0d9ce772231672cbdac5
howardotuya <howardotuya17@gmail.com>
2024-01-03 09:47:34 +0100

    update - vimeo

--- app/case-study/tasty-and-yummy/page.js
+++ app/case-study/tasty-and-yummy/page.js
@@ -388,7 +388,7 @@ export default function TAY() {
               {/* UT 1.2 - VIDEO EMBEDDING */}
               <div className="pt-[56%] relative">
                 <iframe
-                  src="https://player.vimeo.com/video/899048663?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                  src="https://player.vimeo.com/video/899431979?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
                   frameborder="0"
                   allow="autoplay; fullscreen; picture-in-picture"
                   className="absolute top-0 left-0 w-full h-full"
