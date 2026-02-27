cdeb169e09901413241164d87a8dbfc370f50306
Howard Otuya <howardotuya17@gmail.com>
2026-02-27 07:27:28 +0100

    fix: Text dev check

--- components/button/button-secondary.js
+++ components/button/button-secondary.js
@@ -19,7 +19,7 @@ export default function ButtonSecondary({
     "h-12 md:h-16 px-4 md:px-6",
     "flex items-center justify-center text-[#2F3434] rounded-full",
     "border",
-    text === "Under development"
+    text === "Under Development"
       ? "cursor-not-allowed border-[#AFB0AB]"
       : "border-[#DC511A]",
     additionalStyle,
